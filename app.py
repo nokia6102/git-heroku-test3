51759ff7b16405f378cac56c19eca3951c68af1e
nokia6102 <nokia6102@gmail.com>
2021-10-30 10:18:46 +0800

    at 10:20

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def job_function2():
     records = data['records']
     for item in records:
         if item['County']=='高雄市' and item['SiteName']=='鳳山':
-            send_line('[MAXTEST-wakup-auto_10:15]%s>> AQI=%s' %(item['SiteName'], item['AQI']))
+            send_line('[MAXTEST-wakup-auto_10:20]%s>> AQI=%s' %(item['SiteName'], item['AQI']))
 
 def start_scheduler():
     scheduler = BackgroundScheduler()
@@ -97,7 +97,7 @@ def start_scheduler():
     scheduler.add_job(job_wakeup, 'cron', minute='*/10')
 
     # 每天早上6:30執行
-    scheduler.add_job(job_function2, 'cron', hour='10', minute='15')
+    scheduler.add_job(job_function2, 'cron', hour='10', minute='20')
     # scheduler.add_job(job_function2, 'cron', minute='*/1')
 
     # start the scheduler
