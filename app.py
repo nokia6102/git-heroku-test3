75a590a3bf3a2a0b3c76c1c483ddbf31e9ddfb95
nokia6102 <nokia6102@gmail.com>
2021-11-06 11:00:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -142,7 +142,8 @@ def aqi_chart_24h():
         aqi_list.append( float(item['aqi']) )
 
     # plot
-    plt.plot(tm_start,aqi_list,'b-o')
+    plt.plot(aqi_list,'b-o')
+    plt.ylabel('time')  #y軸說明文字
     plt.grid()
     plt.savefig('img.png')
     plt.close()
