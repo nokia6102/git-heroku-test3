bec5a024e5a42df858c10f4d2455a2488b550a35
nokia6102 <nokia6102@gmail.com>
2021-11-06 11:55:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -69,9 +69,9 @@ def map_kh_parking():
     return send_file('./map_kh_parking.html')
 
 
-@app.route("/blog", methods=['GET'])
+@app.route("/homepage", methods=['GET'])
 def blog_index():
-    return app.send_static_file('/blog/index.html')
+    return app.send_static_file('blog/index.html')
 
 @app.route("/map/w01-6", methods=['GET'])
 def map_w01_6():
