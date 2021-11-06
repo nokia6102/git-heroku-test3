22a826133a8ccaa55073d0731da4c3ce3c029a62
nokia6102 <nokia6102@gmail.com>
2021-11-06 11:10:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -138,12 +138,14 @@ def aqi_chart_24h():
     data = resultProxy.fetchall()
 
     aqi_list = list()
+    time_list = list()
+    
     for item in data:
         aqi_list.append( float(item['aqi']) )
-
+        time_list.append(item['time']) )
+       
     # plot
-    plt.plot(dt2,aqi_list,'b-o')
-#    plt.ylabel('time')  #y軸說明文字
+    plt.plot(time_list,aqi_list,'b-o')
     plt.grid()
     plt.savefig('img.png')
     plt.close()
